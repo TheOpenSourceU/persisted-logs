fef9a82404c81abdfe3e0cdad5e68505811e78a3
Frank Villasenor <frank@theOpenSourceU.org>
2024-07-05 18:29:11 -0500

    Gah!

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,41 @@
 import Sqlite3Logger from "./datastore/Sqlite3Logger";
 
-export type BetterLog = {
-  debug(tag: string, msg: string): Promise<void>;
-  info(tag: string, msg: string): Promise<void>;
-  warn(tag: string, msg: string): Promise<void>;
-  error(tag: string, msg: string): Promise<void>;
-};
+// export type BetterLog = {
+//   debug(tag: string, msg: string): Promise<void>;
+//   info(tag: string, msg: string): Promise<void>;
+//   warn(tag: string, msg: string): Promise<void>;
+//   error(tag: string, msg: string): Promise<void>;
+// };
 
 
 function getBetterLog() {
   const db = new Sqlite3Logger('log.db', false);
-  void db.Debug('BetterLogs', 'Bootstrapped');
-  const d: BetterLog = {
-    async debug(tag:string, msg:string) {
+  //void db.Debug('BetterLogs', 'Bootstrapped');
+  // const d = {
+  //   async debug(tag:string, msg:string) {
+  //     return db.Debug(tag, msg);
+  //   },
+  //   async info(tag:string, msg:string) {
+  //     return db.Info(tag, msg);
+  //   },
+  //   async warn(tag:string, msg:string) {
+  //     return db.Warn(tag, msg);
+  //   },
+  //   async error(tag:string, msg:string) {
+  //     return db.Error(tag, msg);
+  //   }
+  // };
+  const d = {
+    debug: async function(tag:string, msg:string) {
       return db.Debug(tag, msg);
     },
-    async info(tag:string, msg:string) {
+    info: async function(tag:string, msg:string) {
       return db.Info(tag, msg);
     },
-    async warn(tag:string, msg:string) {
+    warn: async function(tag:string, msg:string) {
       return db.Warn(tag, msg);
     },
-    async error(tag:string, msg:string) {
+    error: async function(tag:string, msg:string) {
       return db.Error(tag, msg);
     }
   };
@@ -30,3 +44,4 @@ function getBetterLog() {
 
 const betterLog = getBetterLog();
 export default betterLog;
+export { betterLog as BetterLog };
