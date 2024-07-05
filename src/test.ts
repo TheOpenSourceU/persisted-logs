d23a68a218fb56f42274c223c2af266602a659ff
Frank Villasenor <frank@theOpenSourceU.org>
2024-07-04 19:09:30 -0500

    Working now. Basics.

--- src/test.ts
+++ src/test.ts
@@ -1,18 +1,20 @@
 import Sqlite3Logger from "./datastore/Sqlite3Logger";
 
 async function test() {
-  const d = Date.now().toString(32);
+  const d = "001";
   const f = new Sqlite3Logger(`logs-${d}.db`, true);
 
   f.setLogLevel('debug');
 
-  for(let i = 0; i < 1000; i++) {
-    await f.Info(`db-file`, `logs-${d}.db`)
-    await f.Debug('debug-testing', `[${i}] this is a debug test`);
-    await f.Info('info-testing', `[${i}] this is an info test`);
-
-    await f.Error('error-testing', `[${i}] this is an error test`);
-    await f.Warn('warn-testing', `[${i}] this is a warn test`);
+  for(let i = 0; i < 10000000; i++) {
+    const p = [
+      f.Info(`db-file`, `logs-${d}.db`),
+      f.Debug('debug-testing', `[${i}] this is a debug test`),
+      f.Info('info-testing', `[${i}] this is an info test`),
+      f.Error('error-testing', `[${i}] this is an error test`),
+      f.Warn('warn-testing', `[${i}] this is a warn test`)
+    ];
+    await Promise.all(p);
   }
 }
 
