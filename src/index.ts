7d77a0faae9e992b5f6c7d7a39761708a48440d1
Frank Villasenor <frank@theOpenSourceU.org>
2024-07-05 16:47:57 -0500

    License, updates and type tweaks.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import Sqlite3Logger from "./datastore/Sqlite3Logger";
 
+export type BetterLog = { debug(tag: string, msg: string): Promise<void>; info(tag: string, msg: string): Promise<void>; warn(tag: string, msg: string): Promise<void>; error(tag: string, msg: string): Promise<void>; }
+
+let obj: BetterLog;
 export default function () {
   const db = new Sqlite3Logger('log.db', false);
-
-  return {
+  void db.Debug('BetterLogs', 'Bootstrapped');
+  if(obj) return obj;
+  return (obj = {
     async debug(tag:string, msg:string) {
       return db.Debug(tag, msg);
     },
@@ -16,5 +20,5 @@ export default function () {
     async error(tag:string, msg:string) {
       return db.Error(tag, msg);
     }
-  };
+  });
 }
