7e5b53abc1b1d34303ee54b2884acb753adb4f84
Frank Villasenor <frank@theOpenSourceU.org>
2024-08-31 16:26:40 -0500

    Fixed up.

--- test.ts
+++ test.ts
@@ -4,12 +4,14 @@ import BetterLog from "./src/app";
 (async function() {
   const bl = new BetterLog({ dbName:"unit-test-2.db",silent: false });
   try {
-    await bl.info(['test'], 'This is an info message.');
+    await bl.info(['test', 'alina'], `This is an info message. ${Date.now()}`);
 
-    await bl.debug(['debug','test'], 'This is a debug message');
-    await bl.error(['test', 'error'], 'This is an error message. it gets highlighted as an error');
+    await bl.debug(['debug','test', 'Alina'], 'This is a debug message');
+    await bl.error(['test', 'error', ' spaces-And-caps-SHOULD-be-lower-case'], `This is an error message. it gets ${Date.now()}` +
+      ' highlighted as an' +
+      ' error');
 
-    await bl.warn(['test', 'warn'], 'This is a warning message. it gets highlighted as a warning');
+    await bl.warn(['test', 'warn', 'spaces are ok'], `This is a warning message. it gets highlighted as a warning ${Date.now()}`);
   } catch (er) {
     const _null = "[null]";
     console.error(er?.toString() || _null);
