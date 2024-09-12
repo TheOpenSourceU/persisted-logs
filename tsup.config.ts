96f239cdbc94de64aa3b464191f17a5627da261c
Frank Villasenor <frank@theOpenSourceU.org>
2024-09-11 22:34:08 -0500

    Version bump. Update tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,30 +2,17 @@ import { defineConfig, Options } from "tsup";
 
 const env = process.env?.NODE_ENV || 'development';
 
-const tsup: Options = {
-
-  clean: true, // clean up the dist folder
-  // dts: true, // generate dts files
-  // format: ['cjs', 'esm'], // generate cjs and esm files
-  // entryPoints: ['src/index.ts'],
-  //entry: ['src/**/*.ts'], //include all files under src
-  // minify: env === 'production',
-  // bundle: env === 'production',
-  //skipNodeModulesBundle: true,
-  //watch: env === 'development',
-  //target: 'es2020',
-  outDir: env === 'production' ? 'dist' : 'dist',
-  // treeshake: env === 'production',
-  // clean: true,
-};
-
 export default defineConfig({
-  ...tsup,
+  clean: true, // clean up the dist folder
   entry: ["src/app.ts", "src/test.ts"],
   format: ["esm", "cjs"], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
   splitting: false,
-  sourcemap: true, //env === 'development' ? 'inline' : true,
-  //metafile: true,
+  sourcemap: env === 'development' ? 'inline' : false,
+
   watch: env === 'development',
+  
+  minify: env === 'production',
+  treeshake: env === 'production',
+  outDir: env === 'production' ? 'dist' : 'dist',
 });
