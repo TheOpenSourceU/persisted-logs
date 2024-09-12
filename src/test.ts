3f8a54929670c5bd8e48da74c052f504e245acc4
Frank Villasenor <frank@theOpenSourceU.org>
2024-09-11 22:33:46 -0500

    instance checking.

--- src/test.ts
+++ src/test.ts
@@ -10,8 +10,10 @@ for (const path of dbPath) {
   }
 }
 
-(async function() {
+let instance1: BetterLog, instance2: BetterLog;
+const p1 = (async function() {
   const bl = new BetterLog({ dbName: "unit-test-3.db", silent: false });
+  instance1 = bl;
   try {
     await bl.info(["demo", "test", "alina"], `This is an info message. ${Date.now()}`);
 
@@ -26,11 +28,11 @@ for (const path of dbPath) {
   }
 
   try {
-    bl.hush();
+    //bl.hush();
     await bl.info(["demo", "hush test", "hush-test"], `This is the hush demo. When you hush, it silences all output to the console. If you call hushNext(), it will only silence the next log message.`);
     await bl.debug(["demo", "hush test", "hush-test"], "The hush method will continue across all log levels until you call unhush().");
     await bl.error(["demo", "hush test", "hush-test"], "This includes error messages. Again, all of these still go to the persistent log.");
-    bl.unhush();
+    //bl.unhush();
     await bl.info(["demo", "hush test", "hush-test", "unhush"], `Since Unhush has been called, this will go to the console.`);
   } catch (er) {
     const _null = "[null]";
@@ -42,23 +44,22 @@ for (const path of dbPath) {
     const ee = new EventEmitter();
     ee.on("tick", () => {
       try {
-        bl.warn(["tick", "warn"], `event emmiter tick log `).catch(er => console.error(er?.toString()));
+        void bl.warn(["tick", "warn"], `event emitter tick log `).catch(er => console.error(er?.toString()));
       } catch (er) {
         console.error(er?.toString());
       }
     });
+    // ee.on("tick", async () => {
+    //   try {
+    //     await bl.warn(["tick", "warn"], `event emitter tick log `).catch(er => console.error(er?.toString()));
+    //   } catch (er) {
+    //     console.error(er?.toString());
+    //   }
+    // });
     const loops = 9999;
     let hushToggle = false;
     for (let i = 0; i < loops; i++) {
       ee.emit("tick");
-      if (i % 100 === 0) {
-        hushToggle = !hushToggle;
-        if (hushToggle) {
-          bl.hush();
-        } else {
-          bl.unhush();
-        }
-      }
       await bl.debug(["demo", "loop-test"], `Loop test ${i} of ${loops} (${i / loops}) ${hushToggle}`);
     }
   } catch (er) {
@@ -71,9 +72,9 @@ for (const path of dbPath) {
 })();
 
 
-(async function() {
+const p2 = (async function() {
   const bl = new BetterLog({ dbName: "no-close.db", silent: false });
-
+  instance2 = bl;
   try {
     const ee = new EventEmitter();
     ee.on("tick", () => {
@@ -87,14 +88,6 @@ for (const path of dbPath) {
     let hushToggle = false;
     for (let i = 0; i < loops; i++) {
       ee.emit("tick");
-      if (i % 100 === 0) {
-        hushToggle = !hushToggle;
-        if (hushToggle) {
-          bl.hush();
-        } else {
-          bl.unhush();
-        }
-      }
       await bl.debug(["demo", "loop-test", "no-close"], `no-close Loop test ${i} of ${loops} (${i / loops}) ${hushToggle}`);
     }
   } catch (er) {
@@ -106,3 +99,11 @@ for (const path of dbPath) {
   //What happens if we do not close?
   //await bl.Close();
 })();
+
+Promise.all([p1, p2]).then(() => {
+  console.log("All done.");
+  console.log(instance1 != instance2);
+  console.log(instance1 !== instance2)
+}).catch(er => {
+  console.error(er?.toString());
+});
