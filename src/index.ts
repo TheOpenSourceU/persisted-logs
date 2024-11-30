a49263a57d0010181670787412b8d97dc4a39191
Frank Villasenor <frank@theOpenSourceU.org>
2024-11-29 22:58:34 -0600

    Ok, good.

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,34 @@ import BetterLog2 from "./app";
     console.log(_er.message);
   }
 
+  const outputAll = async (n: number = 0) => {
+    try {
+      await bl.info(["test", "info", "outputAll", `outputAll-${n}`], `This is an info ${n}`);
+      await bl.warn(["test", "warn", "outputAll", `outputAll-${n}`], `This is a warning ${n}`);
+      await bl.error(["test", "error", "outputAll", `outputAll-${n}`], `This is an error ${n}`);
+      await bl.debug(["test", "debug", "outputAll", `outputAll-${n}`], `This is a debug ${n}`);
+      await bl.log(["test", "log", "outputAll"], `This is a log ${n}`);
+    } catch (er) {
+      const _er = er as Error;
+      console.log(`two follow up ${n}`);
+      console.log(_er.stack);
+      console.log(_er.message);
+    }
+  };
+
+  await outputAll();
+
+  try {
+    for (let i = 0; i < 100; i++) {
+      await outputAll(i);
+    }
+  } catch(er) {
+    const _er = er as Error;
+    console.log("two follow up");
+    console.log(_er.stack);
+    console.log(_er.message);
+  }
+
   await bl.Close();
 
   process.exit(0);
