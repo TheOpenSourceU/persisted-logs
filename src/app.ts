73065b3fd8beeac99ee3157b08497e1d7b7c74ca
Frank Villasenor <frank@theOpenSourceU.org>
2024-12-02 08:43:22 -0600

    Output the config for client diagnostics. Skip password related env vars.

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,19 @@
 import "reflect-metadata";
+import {config} from "dotenv";
 import {PersistedLogV2} from "./stuff";
+
+const dotConfigOutput = config();
+if(dotConfigOutput.parsed) {
+  const reduced = dotConfigOutput.parsed;
+  for (const reducedKey in reduced) {
+    const val = reduced[reducedKey];
+    const redactedWords = ["password", "pass", "word", "pwd"];
+    const keyToCheck = reducedKey.toLowerCase();
+    const containsRedactedWord = redactedWords.some((word) => keyToCheck.includes(word));
+    if(!containsRedactedWord) {
+      console.debug(`${reducedKey}=${val}`);
+    }
+  }
+}
+
 export default PersistedLogV2;
